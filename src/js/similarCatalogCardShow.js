ce853557ceb2befcece44b6c073ec931dd39884f
Gromova Maria <masia86@mail.ru>
2023-05-31 16:13:34 +0300

    добаление слайдера и обработчика нажатия кнопки,пока со слайдером не разобралась

--- src/js/similarCatalogCardShow.js
+++ src/js/similarCatalogCardShow.js
@@ -34,11 +34,96 @@
     });
   }
 
+
+  // массив содержит элементы с содержимым путей к изображениям для демонстрации товара
+  // т.к. структура может меняться и на данный момент у меня нет необзодимых изображений
+  // т.к. они не предоставлены в макете возьму из изображений с модальными окнами
+  let sourceImgData = [ {
+      name: 'img0',
+      source1: './img/catalog/sofas/D-31/main-img-320.png',
+      source2: './img/catalog/sofas/D-31/main-img-1024.png',
+      source3: './img/catalog/sofas/D-31/main-img.png',
+      source4: './img/catalog/sofas/D-31/main-img-1024.png',
+      src: './img/catalog/sofas/D-31/main-img.png',
+    },
+    {
+      name: 'img1',
+      source1: './img/catalog/sofas/D-31/pre-modal-img-1.png',
+      source2: './img/catalog/sofas/D-31/pre-modal-img-1.png',
+      source3: './img/catalog/sofas/D-31/pre-modal-img-1.png',
+      source4: './img/catalog/sofas/D-31/pre-modal-img-1.png',
+      src: './img/catalog/sofas/D-31/pre-modal-img-1.png',
+    },
+    {
+      name: 'img2',
+      source1: './img/catalog/sofas/D-31/pre-modal-img-2.png',
+      source2: './img/catalog/sofas/D-31/pre-modal-img-2.png',
+      source3: './img/catalog/sofas/D-31/pre-modal-img-2.png',
+      source4: './img/catalog/sofas/D-31/pre-modal-img-2.png',
+      src: './img/catalog/sofas/D-31/pre-modal-img-2.png',
+    },
+    {
+      name: 'img2',
+      source1: './img/catalog/sofas/D-31/pre-modal-img-3.png',
+      source2: './img/catalog/sofas/D-31/pre-modal-img-3.png',
+      source3: './img/catalog/sofas/D-31/pre-modal-img-3.png',
+      source4: './img/catalog/sofas/D-31/pre-modal-img-3.png',
+      src: './img/catalog/sofas/D-31/pre-modal-img-3.png',
+    },
+    {
+      name: 'img2',
+      source1: './img/catalog/sofas/D-31/pre-modal-img-4.png',
+      source2: './img/catalog/sofas/D-31/pre-modal-img-4.png',
+      source3: './img/catalog/sofas/D-31/pre-modal-img-4.png',
+      source4: './img/catalog/sofas/D-31/pre-modal-img-4.png',
+      src: './img/catalog/sofas/D-31/pre-modal-img-4.png',
+    },
+  ]
+
+  // функция получения птуей к изображению, сейчас берет их из заданного массива
+  // в дальнейшем такой код может быть заменен, например, на запрос к серверу
+  function getImgSource (name, sourceData=sourceImgData) {
+    sourceData.forEach(function(elem) {
+      if ( elem.name == name) {
+        return elem;
+      }
+    });
+    return null;
+  }
+
+  // функция для замены основного изображения
+  // требует в качестве входных параметров объект с путями и номер текущего изображения
+  // подразумевается что элемент picture (которую будем изменять) на странице 1!!!
+  // возвращает  текущий номер изображения (который был до замены)
+  function ReplaceProductMainImg(source, num) {
+    let sources = document.querySelectorAll('.product__img-main .product__img-source');
+    let img =  document.querySelector('.product__img-main .product__img');
+    let currentData =  img.getAttribute('data');
+
+    sources[0].setAttribute('srcset', source.source1);
+    sources[1].setAttribute('srcset', source.source2);
+    sources[2].setAttribute('srcset', source.source3);
+    sources[3].setAttribute('srcset', source.source4);
+    img.setAttribute('src', source.src);
+    img.setAttribute('data', num);
+
+    return currentData;
+  }
+
+  // функция получения обекта путей
+  // если номер выходит за границы массива sourceImgData, то получит значения по умолчанию
+  function getSource(num,sourceData=sourceImgData) {
+    if ( (num > 0) && (num < sourceData.length ) ) {
+       return sourceData[num];
+    }
+    return sourceData[0];
+  }
+
   //----------------- основной код скрипта ----------------------
   loadSimilarCards(createSimilarCards(catalogSimilarNum));
 
   /* инициализация слайдера для секции similar  */
-  const usefulSwiper = new Swiper('.similar__swiper-container', {
+  const similarSwiper = new Swiper('.similar__swiper-container', {
     slidesPerView: 1,
     loop: false,
     spaceBetween: 32,
@@ -47,6 +132,7 @@
       // when window width is >= 280px
       280: {
         slidesPerView: 2,
+        spaceBetween: 16,
       },
 
       // when window width is >= 900px
@@ -66,6 +152,75 @@
       prevEl: '.similar__btn-prev',
       },
   });
+
+  //----------------------------------------------------------------------
+  /* инициализация слайдера для секции similar  */
+  const imgPreSwiper = new Swiper('.img-pre__swiper-container', {
+    slidesPerView: 4,
+    loop: false,
+    spaceBetween: 38,
+
+    /*breakpoints: {
+      // when window width is >= 280px
+      280: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+
+      // when window width is >= 900px
+      900: {
+        slidesPerView: 3,
+      },
+
+      // when window width is >= 900px
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+
+    // навигация
+    navigation: {
+      nextEl: '.similar__btn-next',
+      prevEl: '.similar__btn-prev',
+      },*/
+  });
+
+
+  // обработчик события для  просмотра картинок на странице продукта
+
+  document.querySelectorAll('.product__img-preview-btn').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      let img = btn.firstElementChild;
+      let li = btn.parentElement;
+      let num = 0;
+      let currentNum = null;
+      let source = null;
+      //console.log(img);
+
+      if ( img.hasAttribute('data') ) {
+        num = img.getAttribute('data');
+        if (num != NaN) {
+          // получаем пути для замены
+          source = getSource(num);
+          currentNum = ReplaceProductMainImg(source, num);
+          //прячем текущее предварительное изображение
+          if (currentNum != null ) {
+            // у текущего главного  убираем класс для скрытия предварительного изображения
+            let temp = document.querySelector('.product__img-preview-item--hidden');
+            temp.classList.remove('product__img-preview-item--hidden');
+
+            // новому добавляем
+            li.classList.add('product__img-preview-item--hidden');
+
+            // обновляем слайдер!!!!
+            console.log(imgPreSwiper.update());
+          }
+        }
+      }
+    });
+
+  });
+
 
 
 
