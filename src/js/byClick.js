83a17dba83c71d8d81dbe313d4777ae72afa11be
Gromova Maria <masia86@mail.ru>
2023-06-09 11:07:03 +0300

    доделала модальные окна на адаптиве

--- src/js/byClick.js
+++ src/js/byClick.js
@@ -1,5 +1,15 @@
 document.querySelector('.product__btn').addEventListener('click', function(event) {
   // открываем окно просмотра в модальном окне
-  let modal = setModalMode('.modal-form');
+  setModalMode('.modal-form');
   event._isClickWhitinFilter = true; // чтоб не закрыло при всплытии
 })
+
+document.querySelector('.modal-form__btn').addEventListener('click', function(event) {
+  event.preventDefault();
+  // чтоб не путаться предыдущее закрываем
+  closeModal();
+  // новое открываем
+  setModalMode('.modal-info');
+  event._isClickWhitinFilter = true; // чтоб не закрыло при всплытии
+})
+
