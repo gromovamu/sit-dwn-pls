612ceb69b7c20994135b66594755e69267184ab9
Gromova Maria <masia86@mail.ru>
2023-06-14 15:03:50 +0300

    доделала функционал бегунка с ценой

--- src/js/filter.js
+++ src/js/filter.js
@@ -8,30 +8,22 @@ function closeActiveFilter(filterActive='') {
 }
 
 function customListenerForClick(event) {
-  //console.log('customListenerForClick');
-  //console.log(event._isClickWhitinFilter);
-
     if ( event._isClickWhitinFilter ) return;
 
     closeActiveFilter();
-    //console.log('close');
 
     document.removeEventListener('click',customListenerForClick);
 }
 
 function customListenerForClickWithinFilter(event) {
   event._isClickWhitinFilter = true;
-  //console.log('customListenerForClickWithinFilter');
 }
 
 
 document.querySelectorAll('.filter__btn').forEach(function (btn) {
   btn.addEventListener('click', function (event) {
     event.preventDefault();
-    //console.log(this);
     let filter = this.parentElement.querySelector('.filter__container');
-
-      //console.log(filter);
 
     // проверим, если есть другие открытые фильтры закроем, а то у меня так огранизовано,
     // что если на любую кнопку жать то _isClickWhitinFilter тоже установится, и закрытие не сработает
@@ -59,14 +51,40 @@ const positionClick = {
   x: 0,
   y: 0,
   left: 0,
-  top: 0,
-  width: 0,
-  containerWidth: 0,
-  containerHeight: 0,
-  containerLeft: 0,
-  containerTop: 0,
-  moveElem: null,
-  btn: '',
+  btnName: '',
+
+  saveContext(event, btnName) {
+     //сохраняет текущее положение кнопки  как смещение  относительно окна браузера
+    this.x = event.clientX;
+    this.y = event.clientY;
+    this.left = event.clientX-event.offsetX;
+    this.btnName = btnName;
+  },
+
+  reset() {
+    this.x = 0;
+    this.y = 0;
+    this.left = 0;
+    this.btnName = '0';
+  }
+};
+
+const positionFocus = {
+  left: 0,
+  btnName: '',
+  focus: false,
+  saveContext(left, btnName) {
+    //сохраняет текущее положение кнопки  как смещение в процентах относительно внешнего контейнера
+    this.left = left;
+    this.btnName = btnName;
+    this.focus = true;
+  },
+
+  reset() {
+    this.left = 0;
+    this.btnName = '';
+    this.focus = false;
+  }
 };
 
 const runnerState = {
@@ -89,12 +107,13 @@ const runnerState = {
   },
 
   percentInValue(per) {
-    return parseInt((per * (this.maxLimit - this.minLimit)) * 0.01);
+    let value = (per * (this.maxLimit - this.minLimit)* 0.01);
+    return parseInt(value) ;
   },
 
   setCurrMaxLimit(max) {
     max = max > this.maxLimit ? this.maxLimit : max;
-    max = max < this.currMinLimit ? currMinLimit : max;
+    max = max < this.currMinLimit ? this.currMinLimit : max;
 
     this.currMaxLimit = max;
     this.right = this.valueInPercent(max);
@@ -102,7 +121,7 @@ const runnerState = {
 
   setCurrMinLimit(min) {
     min = min < this.minLimit ? this.minLimit : min;
-    min = min > this.currMaxLimit ? currMaxLimit : min;
+    min = min > this.currMaxLimit ? this.currMaxLimit : min;
 
     this.currMinLimit = min;
     this.left = this.valueInPercent(min);
@@ -165,78 +184,147 @@ const runnerState = {
       return this.right;
     }
     return 100;
+  },
+
+  // функция превращает абсолютное смещение по осиX в относительное в процентах (относительно контейнера базовой линии (бегунка))
+  // проценты относительно максимальной ширины бегунка
+  getOffsetLeftInPersent(newLeft) {
+      let containerCoord = document.querySelector(`.${this.classRunnerContainer}`).getBoundingClientRect();
+      let left = ((newLeft - containerCoord.left) * 100.0)/containerCoord.width;
+      return left;
+  },
+
+  // фунция для обработки движения кнопки
+  moveRunnerBtn(left, side) {
+    let btn = null;
+    if( side === 'left') {
+      btn = document.querySelector(`.${this.classRunnerBtnLeft}`);
+    }
+    if( side === 'right') {
+      btn = document.querySelector(`.${this.classRunnerBtnRight}`);
+    }
+
+    if (btn) {
+      let btnCoord = btn.getBoundingClientRect();
+
+      let min = this.getMinRunnerBtnPosition(side);
+      let max = this.getMaxRunnerBtnPosition(side);
+
+      left = left < min ? min: left;
+      left = left > max ? max: left;
+
+      btn.style.left =  `calc(${left}% - ${btnCoord.width/2}px`;
+
+      this.setCurrPosRunner(left, side);
+      this.drawRunnerLine(); //обновим линию бегунка
+      this.updateRunneInputBlock(); // обновим значения в input
+    }
+  },
+
+  // проверяет какая кнопка бегунка выбрана, правая или левая
+  whatIsRunnerBtn(btn) {
+    if ( btn.classList.contains(`${this.classRunnerBtnLeft}`) ) {
+      return 'left';
+    }
+
+    if ( btn.classList.contains(`${this.classRunnerBtnRight}`) ) {
+      return 'right';
+    }
+    return null;
   }
 };
 
+// функции для организации движения мышью
+// обработчики событий от мыши при нажатии и удержании
+function runnerBtnOnMove(event) {
+  let left = runnerState.getOffsetLeftInPersent(event.clientX - positionClick.x + positionClick.left);
+  runnerState.moveRunnerBtn(left, positionClick.btnName);
+}
 
-function onMove(event) {
-  event.preventDefault();
-  if( positionClick.moveElem ) {
-    // я добавила в формулу positionClick.containerLeft,
-    // т.к. у нашего элемента позиционирование абсолютное, относительно родительского элемента
-    let left = event.clientX - positionClick.x + positionClick.left - positionClick.containerLeft;
+function runnerBtnStopMove(event) {
+  document.removeEventListener('mousemove',runnerBtnOnMove);
+  document.removeEventListener('mouseup',runnerBtnStopMove);
+}
 
-    let min = runnerState.getMinRunnerBtnPosition(positionClick.btn);
-    let max = runnerState.getMaxRunnerBtnPosition(positionClick.btn);
-
-    left = (left * 100)/positionClick.containerWidth;
-
-    top = positionClick.top - positionClick.containerTop;
-
-    left = left < min ? min: left;
-    left = left > max ? max: left;
-
-    positionClick.moveElem.style.left =  `calc(${left}% - ${positionClick.width/2}px`;
-    positionClick.moveElem.style.top = `${top}px`
-
-    runnerState.setCurrPosRunner(left, positionClick.btn);
-    runnerState.drawRunnerLine(); //обновим линию бегунка
-    runnerState.updateRunneInputBlock(); // обновим значения в input
+function runnnerMouseEventListener(event) {
+  let btnName = runnerState.whatIsRunnerBtn(this);
+  if (btnName) {
+    positionClick.saveContext(event,btnName);
+    document.addEventListener('mousemove',runnerBtnOnMove);
+    document.addEventListener('mouseup',runnerBtnStopMove);
   }
 }
 
-function stopMove(event) {
-  document.removeEventListener('mousemove',onMove);
-  document.removeEventListener('mouseup',stopMove);
-}
-
-document.querySelectorAll('.runner__btn').forEach(function(filter) {
-  filter.addEventListener('mousedown', function(event) {
-    event.preventDefault();
-    let btnCoord = this.getBoundingClientRect();
-    let containerCoord = this.parentElement.getBoundingClientRect();
-
-    positionClick.moveElem = this;
-
-    positionClick.containerLeft = containerCoord.left;
-    positionClick.containerTop = containerCoord.top;
-    positionClick.containerWidth = containerCoord.width;
-    positionClick.containerHeight = containerCoord.height;
-    positionClick.x = event.clientX;
-    positionClick.y = event.clientY;
-    positionClick.left = btnCoord.left;
-    positionClick.top = btnCoord.top;
-    positionClick.width = btnCoord.width;
-
-    if (this.classList.contains('runner__btn-left')) {
-      positionClick.btn = 'left';
-    }
-
-    if (this.classList.contains('runner__btn-right')) {
-      positionClick.btn = 'right';
-    }
-
-    document.addEventListener('mousemove',onMove);
-    document.addEventListener('mouseup',stopMove);
-  });
+// повесим обработчик на нажатие мыши
+document.querySelectorAll('.runner__btn').forEach(function(btn) {
+  btn.addEventListener('mousedown', runnnerMouseEventListener);
 });
 
+// повесим обработчик с отменой действия на клик, чтобы страница не перезагружалась
 document.querySelectorAll('.runner__btn').forEach(function(filter) {
   filter.addEventListener('click', function(event) {
     event.preventDefault();
   });
 });
 
+// ---------------обработчики событий от клавиатуры-----------------
+function runnerBtnFocus(event) {
+ // event.preventDefault();
+  // console.log('runnerBtnFocus');
+  let btnName = runnerState.whatIsRunnerBtn(this);
+
+  if (btnName) {
+    let btnCoord = this.getBoundingClientRect();
+    let left = btnCoord.left + btnCoord.width/2; // положение центра бегунка
+    left = runnerState.getOffsetLeftInPersent(left); // получим значение в процентах, так удобнее для дальнейших рассетов
+
+    positionFocus.reset();
+    positionFocus.saveContext(left, btnName);
+
+    document.addEventListener('keydown', runnerBtnKeydown);
+    document.addEventListener('blur', runnerBtnBlur);
+  }
+}
+
+function runnerBtnKeydown(event) {
+  let step = runnerState.valueInPercent(10); // вычислим веоичину шага в %
+
+  if ( event.key === 'ArrowRight') {
+    positionFocus.left+=step;
+    runnerState.moveRunnerBtn(positionFocus.left, positionFocus.btnName);
+  }
+  if ( event.key === 'ArrowLeft') {
+    positionFocus.left-=step;
+    runnerState.moveRunnerBtn(positionFocus.left, positionFocus.btnName);
+  }
+}
+
+function runnerBtnBlur(event) {
+  document.removeEventListener('keydown',runnerBtnKeydown);
+  document.removeEventListener('focus',runnerBtnFocus);
+}
+
+// повесим обработчик на кнопку в фокусе
+document.querySelectorAll('.runner__btn').forEach(function(btn) {
+  btn.addEventListener('focus', runnerBtnFocus);
+});
+
+// ------------------ обработчик для ввода в поля цены
+document.querySelector('.filter__input-from').addEventListener('change', function() {
+  let value = Number(this.value);
+  value = isNaN(value) ? runnerState.currMinLimit:value;
+  runnerState.setCurrMinLimit(value);
+  runnerState.update();
+});
+
+document.querySelector('.filter__input-before').addEventListener('change', function() {
+  let value = Number(this.value);
+  value = isNaN(value) ? runnerState.currMaxLimit:value;
+  runnerState.setCurrMaxLimit(value);
+  runnerState.update();
+});
+
+//--------------------- начальная инициализация -----------------
 // Заданная в макете величина не соотносится с линией бегунка с математической точки зрения
 // пересчитаем со значениями указанными в макете, а линию перерисуем
 runnerState.setCurrMaxLimit(150000);
