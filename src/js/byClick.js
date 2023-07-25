7528423b1d60702182ba79720c6dc6e0ba02b2fb
Gromova Maria <masia86@mail.ru>
2023-07-25 09:38:00 +0300

    исправила ошибку в верстке, и точки запятой добавила в js

--- src/js/byClick.js
+++ src/js/byClick.js
@@ -2,7 +2,7 @@ document.querySelector('.product__btn').addEventListener('click', function(event
   // открываем окно просмотра в модальном окне
   setModalMode('modal-form');
   event._isClickWhitinFilter = true; // чтоб не закрыло при всплытии
-})
+});
 
 document.querySelector('.modal-form__btn').addEventListener('click', function(event) {
   event.preventDefault();
@@ -11,5 +11,5 @@ document.querySelector('.modal-form__btn').addEventListener('click', function(ev
   // новое открываем
   setModalMode('modal-info');
   event._isClickWhitinFilter = true; // чтоб не закрыло при всплытии
-})
+});
 
